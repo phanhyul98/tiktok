49ce22d91da3f2afa7750c1aa737f86114eec8f9
yuri <phuonganhd989@gmail.com>
2022-06-05 17:54:53 +0700

    Xây dựng UI Header 6

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -38,6 +38,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
             interactive
             placement="bottom-end"
             delay={[0, 700]}
+            offset={[12, 8]}
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx('menu-popper')}>
