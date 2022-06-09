8ec4d17fe9e36474ef4ec118e6f3580fcceb1ce3
yuri <phuonganhd989@gmail.com>
2022-06-10 00:06:27 +0700

    Xây dựng logic Header 1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,5 +1,6 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from '~/components/Image';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 
@@ -7,10 +8,10 @@ const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
-                src="https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/282195933_1477406302714598_5916748901007568525_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=_CmJ36EgxCMAX-yQeWG&_nc_ht=scontent.fhan2-1.fna&oh=00_AT8ASpf_1NJE4FM5yhpNEM2M70RCvoYWppD3_dwp3zYb6A&oe=6291DE50"
-                alt="Hoaa"
+                src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.6435-9/133112081_2910982859186594_7807663832646570208_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=4RXEgVXidKYAX_Lc42U&_nc_ht=scontent.fhan2-4.fna&oh=00_AT9QssCsdmB3jtE6lsGHWaqsirIC64LOAJl5kLEwm5fx5Q&oe=62C74AC7"
+                alt="Yuri"
             />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,8 @@
-import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import {
-    faCircleXmark,
-    faMagnifyingGlass,
-    faSpinner,
     faEllipsisVertical,
     faCircleQuestion,
     faEarthAsia,
@@ -17,17 +13,14 @@ import {
     faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
-import HeadlessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
-
 import Button from '~/components/Button';
-import { Wrapper as PopperWrapper } from '~/components/Popper';
-import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
 import { UploadIcon } from '~/components/Icons';
 import { MessageIcon } from '~/components/Icons';
 import { InboxIcon } from '~/components/Icons';
 import Image from '~/components/Image';
+import Search from '../Search';
 
 const cx = classNames.bind(styles);
 
@@ -63,14 +56,7 @@ const MENU_ITEMS = [
 ];
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([]);
     const currentUser = true;
-
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([]);
-        }, 0);
-    }, []);
 
     const handleMenuChange = (menuItem) => {
         switch (menuItem.type) {
@@ -109,33 +95,7 @@ function Header() {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok" />
-                <HeadlessTippy
-                    interactive
-                    visible={searchResult.length > 0}
-                    render={(attrs) => (
-                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                            <PopperWrapper>
-                                <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                            </PopperWrapper>
-                        </div>
-                    )}
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Search accounts and videos" spellCheck={false} />
-                        <button className={cx('clear')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-
-                        <button className={cx('search-btn')}>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        </button>
-                    </div>
-                </HeadlessTippy>
+                <Search />
 
                 <div className={cx('actions')}>
                     {currentUser ? (
